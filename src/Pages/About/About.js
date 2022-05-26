131f85f56e4c21418805b99f94c31ace719d568e
pushpitanandy <nandypushpita@gmail.com>
2022-05-26 20:48:32 +0600

    About page updated

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import WhyPrefer from '../Home/WhyPrefer';
 
 const About = () => {
     return (
-        <div>
-            <h2>This is About</h2>
+        <div classname='px-12 my-12'>
+            <h1 className="my-5 text-center text-5xl font-bold">Hand Tools Manufacturer <br /><span className='text-primary m-5'>BD Tools Industries</span></h1>
+            <p className="px-12 my-5 text-xl">Get your next Top Quality tools from us which are made of top quality materials manipulating state-of-art techniques. This permits maximum degree of safety, comfort and user-friendliness.</p>
+            <WhyPrefer></WhyPrefer>
         </div>
     );
 };
