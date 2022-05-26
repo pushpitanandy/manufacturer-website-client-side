0634195bd9ae41a7844989fe4ca1a266a9b86a95
pushpitanandy <nandypushpita@gmail.com>
2022-05-26 17:51:44 +0600

    Business summary edited

--- src/Pages/Home/BusinessSummary.js
+++ src/Pages/Home/BusinessSummary.js
@@ -1,36 +1,39 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFlag, faHandshake, faTruck, faCogs } from '@fortawesome/free-solid-svg-icons';
+import { faFlag, faTruck, faUserGroup, faThumbsUp } from '@fortawesome/free-solid-svg-icons';
 
 const BusinessSummary = () => {
     return (
         <div>
             <h2 className="text-5xl text-center font-extrabold m-10">Businesses trust <span className='text-primary'>BD TOOLS</span></h2>
-            <div className="m-10 grid grid-cols-6 gap-6 text-center">
+            <div className="m-10 grid grid-cols-4 gap-6 text-center">
                 <div>
                     <div className="text-primary p-2">
                         <FontAwesomeIcon icon={faFlag} size="4x" />
                     </div>
-                    <h4 className='text-3xl text-lime-900'>72</h4>
-                    <p>Countries</p>
-                </div>
-                <div>
-                    <div className="text-primary p-2">
-                        <FontAwesomeIcon icon={faHandshake} size="4x" />
-                    </div>
-                    <p>Transparancy in all dealings</p>
+                    <h4 className='text-3xl text-lime-700 font-bold'>72</h4>
+                    <p className='text-lg'>Countries</p>
                 </div>
                 <div>
                     <div className="text-primary p-2">
                         <FontAwesomeIcon icon={faTruck} size="4x" />
                     </div>
-                    <p>Competitive pricing and deliveries on time</p>
+                    <h4 className='text-3xl text-lime-700 font-bold'>500+</h4>
+                    <p className='text-lg'>Deliveries</p>
                 </div>
                 <div>
                     <div className="text-primary p-2">
-                        <FontAwesomeIcon icon={faCogs} size="4x" />
+                        <FontAwesomeIcon icon={faUserGroup} size="4x" />
                     </div>
-                    <p>Using modern technique and standards to make product</p>
+                    <h4 className='text-3xl text-lime-700 font-bold'>200+</h4>
+                    <p className='text-lg'>Clients</p>
+                </div>
+                <div>
+                    <div className="text-primary p-2">
+                        <FontAwesomeIcon icon={faThumbsUp} size="4x" />
+                    </div>
+                    <h4 className='text-3xl text-lime-700 font-bold'>100+</h4>
+                    <p className='text-lg'>Reviews</p>
                 </div>
             </div>
         </div >
